393e3895b98e6fb05dadb399edf4a9919f4d7ae3
tuaniVan <tuamphann5@gmail.com>
2024-05-09 23:40:38 +0700

    call api search and filter

--- src/components/Recruitment/personalNeeds.js
+++ src/components/Recruitment/personalNeeds.js
@@ -7,11 +7,14 @@ import "../../assets/css/cssRecruitment/recruitment.css";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css"
 import ClearIcon from '@mui/icons-material/Clear';
 import imagePractice from '../../assets/image/PracticeImage.jpg'
-
+import CreateIcon from '@mui/icons-material/Create';
 import { useState } from "react";
 import DialogPersonalForm from "./dialog/dialogPersonalForm";
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+
 import DialogPersonalFormUpdate from "./dialog/dialogPersonalFormUpdate";
 import DialogPersonalFormWatch from "./dialog/dialogPersonalFormWatch";
+import axios from "axios";
 export default function PersonalNeeds() {
     const [open, setOpen] = useState(false);
 
@@ -23,28 +26,60 @@ export default function PersonalNeeds() {
     }
 
 
-
-    const [status, setStatus] = useState('');
-    const handleChange = (e) => {
-        setStatus(e.target.value);
-        console.log(e.target.value)
+    const [valueRecuitments, setSearchName] = useState('');
+    const [recruitments, setSearchResults] = useState([]);
+    const [showError, setShowError] = useState(false);
+    const handleSearch = (event) => {
+        setSearchName(event.target.value);
     };
-    // Dữ liệu fake của thằng trạng thái, nhân sự
+
+    const handleSubmitSearch = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.get(`http://localhost:8080/api/recruitmentRequests/search?name=${valueRecuitments}`);
+            setSearchResults(response.data);
+            console.log(recruitments);
+            if (response.data.length === 0) {
+                setShowError(true);
+            } else {
+                setShowError(false);
+            }
+        } catch (error) {
+            console.error('Error search results:', error);
+        }
+    };
+
     const listTestSelect = [
-        { id: 1, text: "Hoạt động" },
-        { id: 2, text: "Không hoạt động" },
-        { id: 3, text: "Đang chờ" }
+        { id: "Đã hủy", text: "Đã hủy" },
+        { id: "Đã xác nhận", text: "Đã xác nhận" },
+        { id: "Đang chờ", text: "Đang chờ" },
+        { id: "Bị từ chối bởi DET", text: "Bị từ chối bởi DET" },
+        { id: "Bị từ chối bởi DCAN", text: "Bị từ chối bởi DCAN" },
+        { id: "Đang tuyển dụng", text: "Đang tuyển dụng" },
+        { id: "Đã bàn giao", text: "Đã bàn giao" },
     ]
-    
-    const listPersonal = [
-        { id: 1, name: 'DECEN- Nhu cầu nhân sự quý 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' },
-        { id: 2, name: 'DECEN- Nhu cầu 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' },
-        { id: 3, name: 'DECEN- Nhu cầu 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' },
-        { id: 4, name: 'DECEN- Nhu cầu 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' },
-        { id: 5, name: 'DECEN- Nhu cầu 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' },
-        { id: 6, name: 'DECEN- Nhu cầu 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' },
-        { id: 7, name: 'DECEN- Nhu cầu 3', time: '14:19 11/11/2019', status: 'Đã gửi', man: 'KongDT' }
-    ]
+    const [selectedStatus, setSelectedStatus] = useState('');
+    const handleStatusChange = (event) => {
+        setSelectedStatus(event.target.value);
+        handleSubmitSelect(event.target.value);
+    };
+
+    const handleSubmitSelect = async (selectedValue) => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/recruitmentRequests/filter?status=${selectedValue}`);
+            setSearchResults(response.data);
+            if (response.data.length === 0) {
+                setShowError(true);
+            } else {
+                setShowError(false);
+            }
+        } catch (error) {
+            console.error('Error searching by status:', error);
+        }
+    };
+
+
+
 
     return (
         <>
@@ -91,21 +126,23 @@ export default function PersonalNeeds() {
                         <div className="d-flex justify-content-between">
                             <div className="d-flex">
                                 <div className="search-input position-relative">
-                                    <input type="text" className="border-clr-grey w-px position-relative" placeholder="Tìm kiếm với tên..." />
-                                    <svg className="search-icon position-absolute" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="rgb(131 125 125 / 87%)" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" /></svg>
+                                    <form onSubmit={handleSubmitSearch}>
+                                        <input type="text" className="border-clr-grey w-px position-relative" name="name" value={valueRecuitments} onChange={handleSearch} placeholder="Tìm kiếm với tên..." />
+                                        <svg className="search-icon position-absolute" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="rgb(131 125 125 / 87%)" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" /></svg>
+                                    </form>
                                 </div>
                                 <FormControl className="ml-10" id="select-form" sx={{ minWidth: 300 }}>
                                     <InputLabel htmlFor="grouped-select">Trạng thái</InputLabel>
                                     <Select defaultValue=""
                                         id="grouped-select"
                                         label="Grouping"
-                                        value={status}
-                                        onChange={handleChange}
+                                        onChange={handleStatusChange}
+                                        value={selectedStatus}
                                     >
-                                        {listTestSelect.map(item => (
-                                            <MenuItem value={item.id} key={item.id}>{item.text}</MenuItem>
-                                        ))
-                                        }
+                                        {
+                                            listTestSelect.map(item => (
+                                                <MenuItem value={item.id} key={item.status} onClick={handleSubmitSelect}>{item.text}</MenuItem>
+                                            ))}
                                     </Select>
                                 </FormControl>
                             </div>
@@ -124,20 +161,22 @@ export default function PersonalNeeds() {
                                 <th className="w-130 text-center">Người gửi</th>
                                 <th className="w-130 text-right">Hành động</th>
                             </tr>
-                            {listPersonal.map(item => (
+                            {recruitments.map(item => (
                                 <tr className="grey-text count-tr" key={item.id}>
-                                    <td className="count-td"></td>
-                                    <td className=" setWidth">{item.name}</td>
-                                    <td className=" text-center">{item.time}</td>
+
+                                    <td className="count-td">{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.dateStart}</td>
                                     <td className=" text-center">{item.status}</td>
-                                    <td className=" text-center">{item.man}</td>
-                                    <td className="p-tricklord text-right">
-                                        <DialogPersonalFormWatch/>
-                                        <DialogPersonalFormUpdate />
+                                    <td className=" text-center">{item.users.name}</td>
+                                    <td className=" text-center">
+                                        <RemoveRedEyeIcon className="color-blue white-div font-size-large" />
+                                        <CreateIcon className="color-orange pencil-btn font-size-medium" />
                                     </td>
                                 </tr>
                             ))}
                         </table>
+                        {showError && <span>No Content</span>}
                     </div>
                 </div>
             </Box>
